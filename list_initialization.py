90ffc64e8bf5fe356f33400e66039e884dbcbac6
Aayush Naik <aayushnaik17@gmail.com>
2020-07-21 12:49:51 -0700

    Change variable names

--- list_initialization.py
+++ list_initialization.py
@@ -5,16 +5,19 @@ from perf_utils import Timer
 
 
 def list_1d(list_size):
+    with Timer("1d_product_none"):
+        a = [None] * list_size
+
     with Timer("1d_product"):
-        a = [0] * list_size
+        b = [0] * list_size
 
     with Timer("1d_list_comprehension"):
-        a = [0 for _ in range(list_size)]
+        c = [0 for _ in range(list_size)]
 
     with Timer("1d_append"):
-        a = []
+        d = []
         for _ in range(list_size):
-            a.append(0)
+            d.append(0)
 
 
 def list_2d(nrows, ncols, suffix=""):
@@ -22,10 +25,10 @@ def list_2d(nrows, ncols, suffix=""):
         a = [[0] * ncols for _ in range(nrows)]
 
     with Timer(f"2d_list_comprehension{suffix}"):
-        a = [[0 for _ in range(ncols)] for _ in range(nrows)]
+        b = [[0 for _ in range(ncols)] for _ in range(nrows)]
 
 
 if __name__ == "__main__":
-    # list_1d(int(1e7))
-    list_2d(100000, 1000, "_more_rows")
-    list_2d(1000, 100000, "_more_cols")
+    list_1d(int(1e7))
+    # list_2d(100000, 1000, "_more_rows")
+    # list_2d(1000, 100000, "_more_cols")
